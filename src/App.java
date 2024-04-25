048d8ab6bcffa9ec9731cb6ca1454c407f1df4b7
Simon Nestler <simon.nestler@thi.de>
2024-04-25 12:12:05 +0200

    Arbeiten mit einer Liste mit Strings

--- src/App.java
+++ src/App.java
@@ -1,5 +1,41 @@
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        ArrayList<String> list = new ArrayList<String>();
+
+        String exklamation = "!";
+
+        list.add("Hallo");
+        list.add("Welt");
+        list.add(exklamation);
+        list.add("Wie");
+        list.add("geht's");
+        list.add("?");
+        list.add(exklamation);
+        list.add(exklamation);
+        list.add(exklamation);
+
+        System.out.println(list);
+
+        list.remove(0);
+        list.remove(exklamation);
+
+        System.out.println(list);
+
+        System.out.println("Das erste Element: " + list.getFirst());
+        System.out.println("Das letzte Element: " + list.getLast());
+        System.out.println("Das dritte Element: " + list.get(2));
+
+        list.set(2, "Mars");
+
+        System.out.println(list);
+
+        System.out.println("Die Länge der Liste: " + list.size());
+        System.out.println("Ist Hallo in der Liste? " + list.contains("Hallo"));
+        System.out.println("An welcher Stelle steht das erste !? " + list.indexOf(exklamation));
+        System.out.println("An welcher Stelle steht das letzte !? " + list.lastIndexOf(exklamation));
+
     }
 }
